e5e12f7fb1e92541300c4ae4eba2fd0f59904e01
Jose <joseurio150@gmail.com>
2023-02-13 17:26:29 -0500

    Starter code for Account class

---
+++ Account.java
@@ -0,0 +1,47 @@
+public class Account{
+    public String displayName;
+    private String username;
+    private String password;
+    public Number likeCount;
+    public Number postCount;
+    private String[] recentPosts;
+    private String[] likedPosts;
+    private float[] lastLocation;
+
+    public void createAccount(){
+        //Create Account
+    }
+
+    public void signIn(){
+        //Sign in
+    }
+
+    public void deleteAccount(){
+        //Delete Acount
+    }
+
+    public void changeDisplayName(){
+        //Change display name
+    }
+
+    public void getLocation(){
+        //Get location
+    }
+
+    public void makePost(){
+        //Make post
+    }
+
+    public void getRecentPosts(){
+        //Get recent posts
+    }
+
+    public void getLikedPosts(){
+        //Get liked posts
+    }
+
+    public void makeComment(){
+        //Make comment
+    }
+
+}
