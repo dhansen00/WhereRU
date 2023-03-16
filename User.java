ea337b4ff45cf37129a4581233d03e3302cd1b9e
Jose <joseurio150@gmail.com>
2023-03-16 15:34:46 -0400

    added psql query for get recent posts

--- User.java
+++ User.java
@@ -1,3 +1,7 @@
+import java.text.*;
+import java.util.*;
+import java.sql.*;
+
 public class User {
     public String username = null;
 
@@ -5,8 +9,17 @@ public class User {
         username = user;
     }
     public void makePost(String text, Double[] location, int radius, String[] tags){
-        //get current time
-        //Database.insertPost(username, text, "TIME", location, radius, tags);
+        Calendar cal = Calendar.getInstance();
+        Date date=cal.getTime();
+        DateFormat dateFormat = new SimpleDateFormat("HHmmss");
+        int postTime = Integer.parseInt(dateFormat.format(date));
+        try{
+            Database.insertPost(username, text, postTime, location, radius, tags);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        //return post object?
     }
     public void makeComment(){
         //Make comment
@@ -17,7 +30,8 @@ public class User {
     }
 
     public void getRecentPosts(){
-        //Get recent posts
+        ResultSet r = Database.query("SELECT * FROM posts WHERE author = '"+ username +"' ORDER BY posts.id DESC;");
+        //return an array of posts up to x amount of posts
     }
     
     public void getLikedPosts(){
