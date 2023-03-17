f15dc7ed5c127a7633d003727fd4480e9f652e10
Jose <joseurio150@gmail.com>
2023-03-17 17:58:32 -0400

    Finished Database Code

--- Database.java
+++ Database.java
@@ -38,7 +38,7 @@ public class Database {
         }
     }
 
-    public static void insertPost(String username, String text, int timeStamp, Double[] location, int radius, String[] tags) throws Exception{
+    public static boolean insertPost(String username, String text, Long timeStamp, Double[] location, int radius, String[] tags) throws Exception{
         //sets up psql code to be in the form "INSERT INTO table(col1,...) VALUES (?,...)"
         String psql = "INSERT INTO posts VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
         Connection conn = getRemoteConnection();
@@ -69,12 +69,18 @@ public class Database {
         }
         st.setDouble(11,location[0]);
         st.setDouble(12,location[1]);
-        st.setInt(13,timeStamp); //timestamp should be of the format yyyyMMddhhmmss
+        st.setLong(13,timeStamp); //timestamp should be of the format yyyyMMddhhmmss
 
-        st.executeUpdate();
+        int inserted = st.executeUpdate();
+        if (inserted == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
-    public static void insertLogin(String[] values) throws Exception{
+    public static boolean insertLogin(String[] values) throws Exception{
         //sets up psql code to be in the form "INSERT INTO table(col1,...) VALUES (?,...)"
         String psql = "INSERT INTO logins VALUES (";
         for (int i = 0; i < values.length; i++){
@@ -96,16 +102,165 @@ public class Database {
         PreparedStatement st2 = conn.prepareStatement("INSERT INTO \"accountLikes\" VALUES (?,?)");
         st.setString(1, values[0]);
         st.setInt(2, 0);
-        st2.executeUpdate();
+        int inserted = st2.executeUpdate();
+        if (inserted == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }  
     
-    public static void insertComment(){}
+    public static boolean insertComment(String username,int postid,String text,Long timeStamp) throws Exception{
+        String psql = "INSERT INTO comments VALUES (?,?,?,?,?,?)";
 
-    public static void likePost(){}
-    
-    public static void dislikePost(){}
+        //code to get next available id
+        ResultSet r = query("SELECT max(id) FROM comments;");
+        int nextid = 888888;
+        try{
+            r.next();
+            nextid = r.getInt(1);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        nextid++;
 
-    public static void deleteComment(){}
+        Connection conn = getRemoteConnection();
+        PreparedStatement st = conn.prepareStatement(psql);
 
-    public static void deletePost(){}
+        st.setInt(1, nextid);
+        st.setInt(2,postid);
+        st.setString(3, text);
+        st.setInt(4,0);
+        st.setString(5, username);
+        st.setLong(6, timeStamp);
+        int inserted = st.executeUpdate();
+        if (inserted != 1){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean likePost(String authorUsername, int postid) throws Exception{
+        Connection conn = getRemoteConnection();
+        //increments the posts like ctr by 1
+        PreparedStatement st = conn.prepareStatement("UPDATE posts SET likes = likes + 1 WHERE id = " + postid + " AND author = '" + authorUsername + "';");
+        int changed = st.executeUpdate();
+        if (changed != 1){ //if no rows are updated then return false *only one should be
+            return false;
+        }
+
+        //increments the post authors like ctr by 1
+        st = conn.prepareStatement("UPDATE \"accountLikes\" SET likes = likes + 1 WHERE username = '" + authorUsername + "';");
+        changed = st.executeUpdate();
+        if (changed != 1){ //if no rows are updated then return false *only one should be
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean likeComment(String authorUsername, int commentid) throws Exception{
+        Connection conn = getRemoteConnection();
+        //increments the comments like ctr by 1
+        PreparedStatement st = conn.prepareStatement("UPDATE comments SET likes = likes + 1 WHERE id = " + commentid + " AND author = '" + authorUsername + "';");
+        int changed = st.executeUpdate();
+        if (changed != 1){ //if no rows are updated then return false *only one should be
+            return false;
+        }
+
+        //increments the comment authors like ctr by 1
+        st = conn.prepareStatement("UPDATE \"accountLikes\" SET likes = likes + 1 WHERE username = '" + authorUsername + "';");
+        changed = st.executeUpdate();
+        if (changed != 1){ //if no rows are updated then return false *only one should be
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean dislikePost(String authorUsername, int postid) throws Exception{
+        Connection conn = getRemoteConnection();
+        //increments the posts like ctr by 1
+        PreparedStatement st = conn.prepareStatement("UPDATE posts SET likes = likes - 1 WHERE id = " + postid + " AND author = '" + authorUsername + "';");
+        int changed = st.executeUpdate();
+        if(changed != 1){ //if no rows are updated then return false *only one should be
+            return false;
+        }
+        
+        //increments the post authors like ctr by 1
+        st = conn.prepareStatement("UPDATE \"accountLikes\" SET likes = likes - 1 WHERE username = '"+authorUsername+"';");
+        changed = st.executeUpdate();
+        if(changed != 1){ //if no rows are updated then return false *only one should be
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean dislikeComment(String authorUsername, int commentid) throws Exception{
+        Connection conn = getRemoteConnection();
+        //increments the comments like ctr by 1
+        PreparedStatement st = conn.prepareStatement("UPDATE comments SET likes = likes - 1 WHERE id = " + commentid + " AND author = '" + authorUsername + "';");
+        int changed = st.executeUpdate();
+        if(changed != 1){
+            return false;
+        }
+
+        //increments the comment authors like ctr by 1
+        st = conn.prepareStatement("UPDATE \"accountLikes\" SET likes = likes - 1 WHERE username = '" + authorUsername + "';");
+        changed = st.executeUpdate();
+        if(changed != 1){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean deleteComment(String authorUsername, int commentid) throws Exception{
+        Connection conn = getRemoteConnection();
+        
+        //find out how many likes a comment has
+        ResultSet r = query("SELECT likes FROM comments WHERE id = " + commentid + ";");
+        r.next();
+        int deletedLikes = r.getInt(1);
+        System.out.println(deletedLikes);
+        //remove those likes from the author
+        PreparedStatement st = conn.prepareStatement("UPDATE \"accountLikes\" SET likes = likes - " + deletedLikes + " WHERE username = '" + authorUsername + "';");
+        int changed = st.executeUpdate();
+        if(changed != 1){
+            return false;
+        }
+
+        //delete the comment
+        st = conn.prepareStatement("DELETE FROM comments WHERE id = " + commentid + ";");
+        changed = st.executeUpdate();
+        if(changed != 1){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean deletePost(String authorUsername, int postid) throws Exception{
+        //find out how many likes a comment has
+        ResultSet r = query("SELECT likes FROM posts WHERE id = " + postid + ";");
+        r.next();
+        int deletedLikes = r.getInt(1);
+
+        Connection conn = getRemoteConnection();
+
+        //delete the comment
+        PreparedStatement st = conn.prepareStatement("DELETE FROM posts WHERE id = " + postid + " AND author = '" + authorUsername + "';");
+        int changed = st.executeUpdate();
+        if (changed != 1){
+            return false;
+        }
+
+        //remove those likes from the author
+        st = conn.prepareStatement("UPDATE \"accountLikes\" SET likes = likes - " + deletedLikes + " WHERE username = '" + authorUsername + "';");
+        changed = st.executeUpdate();
+        if (changed != 1){
+            return false;
+        }
+
+        return true;
+    }
+
 }
